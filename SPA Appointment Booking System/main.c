829557ac501a0cfced9dd03197d349daafe866f3
camper0g <sarojbro361@gmail.com>
2023-06-03 18:21:48 +0545

    login

--- SPA Appointment Booking System/main.c
+++ SPA Appointment Booking System/main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+#include "variables.c"
+char username, password, uname, pass;
 void login();
 void view_appointment();
 void book_appointment();
@@ -7,7 +11,6 @@ void cancel_appointment();
 void view_transactions();
 void delete_transactions();
 void edit_price();
-int pass;
 int main()
 {
     login();
@@ -74,9 +77,58 @@ int main()
 
 void login()
 {
-    /*printf("Enter Password");
-    scanf("%d",&pass);*/
+	int a=0,i=0;
+    char uname[10],c=' '; 
+    char pword[10],code[10];
+    char user[10]="user";
+    char pass[10]="pass";
+    do
+    {
+        system("cls");
+        
+        printf("\n  =========================  LOGIN FORM  =========================  ");
+        printf(" \n                       ENTER USERNAME:-");
+        scanf("%s", &uname); 
+        printf(" \n                       ENTER PASSWORD:-");
+        while(i<10)
+        {
+            pword[i]=getch();
+            c=pword[i];
+            if(c==13) break;
+            else printf("*");
+            i++;
+        }
+        pword[i]='\0';
+        //char code=pword;
+        i=0;
+        //scanf("%s",&pword); 
+            if(strcmp(uname,"user")==0 && strcmp(pword,"pass")==0)
+        {
+        printf("  \n\n\n       WELCOME TO OUR SYSTEM !!!! LOGIN IS SUCCESSFUL");
+        printf("\n\n\n\t\t\t\tPress any key to continue...");
+        getch();//holds the screen
+        break;
+        }
+        else
+        {
+            printf("\n        SORRY !!!!  LOGIN IS UNSUCESSFUL");
+            a++;
+            
+            getch();//holds the screen
+            
+        }
+    }while(a<=3);
+
+	if (a>2)
+	{
+		printf("\nSorry you have entered the wrong username and password for four times!!!");
+		
+		getch();
+        exit(0);
+		
+	}
 }
+
 
 void view_appointment()
 {
@@ -86,25 +138,30 @@ void view_appointment()
 
 void book_appointment()
 {
+    system("pause");
     system("cls");
 }
 
-void cancel_appointment()
+void cancel_appointment() 
 {
+    system("pause");
     system("cls");
 }
 
 void view_transactions()
 {
+    system("pause");
     system("cls");
 }
 
 void delete_transactions()
 {
+    system("pause");
     system("cls");
 }
 
 void edit_price()
 {
+    system("pause");
     system("cls");
 }
